c1c973296454e8283fecb5cf52de2fbc602472fd
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2024-05-22 14:53:27 +0800

    Update schema.go

--- graphql/service/config/global/schema.go
+++ graphql/service/config/global/schema.go
@@ -29,45 +29,65 @@ func (b *builder) WriteLine(depth int, line string) {
 
 func (b *builder) Build() (string, error) {
 
-	v := reflect.ValueOf(daeConfig.Global{})
-	t := v.Type()
-	b.WriteLine(0, b.Head+" {")
-	for i := 0; i < v.NumField(); i++ {
-		field := v.Field(i)
-		structField := t.Field(i)
-		name, ok := structField.Tag.Lookup("mapstructure")
-		if !ok {
-			return "", fmt.Errorf("field %v has no required mapstructure", structField.Name)
-		}
-		// To lower camel case.
-		name = strcase.LowerCamelCase(name)
-		switch field := field.Interface().(type) {
-		case uint, uint8, uint16, uint32, uint64,
-			int, int8, int16, int32, int64:
-			// Int.
-			switch field.(type) {
-			case uint, uint32, uint64, int64:
-				logrus.WithFields(logrus.Fields{
-					"name": structField.Name,
-					"type": structField.Type.String(),
-				}).Warnln("dangerous converting: may exceeds graphQL int32 range")
-			}
+    v := reflect.ValueOf(daeConfig.Global{})
+    t := v.Type()
+    b.WriteLine(0, b.Head+" {")
+    for i := 0; i < v.NumField(); i++ {
+        field := v.Field(i)
+        structField := t.Field(i)
+        name, ok := structField.Tag.Lookup("mapstructure")
+        if !ok {
+            return "", fmt.Errorf("field %v has no required mapstructure", structField.Name)
+        }
+        // To lower camel case.
+        name = strcase.LowerCamelCase(name)
 
-			b.WriteLine(1, name+": Int"+b.NotNullString)
-		case string:
-			b.WriteLine(1, name+": String"+b.NotNullString)
-		case time.Duration:
-			b.WriteLine(1, name+": Duration"+b.NotNullString)
-		case bool:
-			b.WriteLine(1, name+": Boolean"+b.NotNullString)
-		case []string:
-			b.WriteLine(1, name+": [String!]"+b.NotNullString)
-		default:
-			return "", fmt.Errorf("unknown type: %T", field)
-		}
-	}
-	b.WriteLine(0, "}")
-	return b.sb.String(), nil
+        switch field := field.Interface().(type) {
+        case uint, uint8, uint16, uint32, uint64,
+            int, int8, int16, int32, int64:
+            var intValue int64
+            var err error
+
+            // Safe conversion to int64, then check GraphQL int32 limits
+            intValue, err = convertToInt64(field)
+            if err != nil {
+                return "", fmt.Errorf("error converting %v to int64: %s", structField.Name, err)
+            }
+
+            if intValue < -2147483648 || intValue > 2147483647 {
+                b.WriteLine(1, name+": String"+b.NotNullString)
+                logrus.WithFields(logrus.Fields{
+                    "name": structField.Name,
+                    "type": structField.Type.String(),
+                }).Warnln("value exceeds GraphQL Int32 range, using String type instead")
+            } else {
+                b.WriteLine(1, name+": Int"+b.NotNullString)
+            }
+
+        case string:
+            b.WriteLine(1, name+": String"+b.NotNullString)
+        case time.Duration:
+            b.WriteLine(1, name+": Duration"+b.NotNullString)
+        case bool:
+            b.WriteLine(1, name+": Boolean"+b.NotNullString)
+        case []string:
+            b.WriteLine(1, name+": [String!]"+b.NotNullString)
+        default:
+            return "", fmt.Errorf("unknown type: %T", field)
+        }
+    }
+    b.WriteLine(0, "}")
+    return b.sb.String(), nil
+}
+
+func convertToInt64(val interface{}) (int64, error) {
+    switch v := val.(type) {
+    case uint, uint8, uint16, uint32, uint64,
+        int, int8, int16, int32, int64:
+        return reflect.ValueOf(v).Convert(reflect.TypeOf(int64(0))).Int(), nil
+    default:
+        return 0, fmt.Errorf("invalid type for conversion: %T", v)
+    }
 }
 
 func Schema() (string, error) {
