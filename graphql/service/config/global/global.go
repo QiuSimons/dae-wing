9fc0ea8d70bb6b7f9a1a21c927dcb03852d4f1fb
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2026-03-31 23:13:50 +0800

    Modify go:generate commands in global.go
    Update go:generate commands for resolver and input generation.

--- graphql/service/config/global/global.go
+++ graphql/service/config/global/global.go
@@ -10,6 +10,8 @@ import (
 )
 
 //go:generate go run ./generator/resolver generated_resolver.go
+//go:generate sed -i "s/func (r \*Resolver) () bool/func (r *Resolver) SoMarkFromDaeSet() bool/" generated_resolver.go
 //go:generate go run ./generator/input generated_input.go
+//go:generate sed -i "s/i\. != nil/i.SoMarkFromDaeSet != nil/; s/i\. /i.SoMarkFromDaeSet /; s/\\t\\*bool/SoMarkFromDaeSet\\t*bool/" generated_input.go
 //go:generate go run -mod=mod golang.org/x/tools/cmd/goimports -w generated_resolver.go generated_input.go
 //go:generate go fmt
